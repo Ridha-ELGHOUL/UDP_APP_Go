6d019a2197ce0de7cc11a94a83c46eeac6320ff4
ryda <ridha.elghoul01@gmail.com>
2019-05-06 11:58:06 +0100

    change to func. code

--- udpClient.go
+++ udpClient.go
@@ -1,23 +1,32 @@
-package main
+package net
+
 import (
-    "fmt"
-    "net"
-    "bufio"
+	"bufio"
+	"fmt"
+	"net"
 )
 
+func init_client_server() {
+	p := make([]byte, 2048)
+	conn, err := net.Dial("udp", "127.0.0.1:1234")
+
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	fmt.Printf(" UDP Client connected !!! \n")
+	fmt.Fprintf(conn, "\n Hi UDP Server, How are you doing?")
+	_, err = bufio.NewReader(conn).Read(p)
+	if err == nil {
+		fmt.Printf("%s\n", p)
+	} else {
+		fmt.Printf("Some error %v\n", err)
+	}
+	conn.Close()
+
+}
 func main() {
-    p :=  make([]byte, 2048)
-    conn, err := net.Dial("udp", "127.0.0.1:1234")
-    if err != nil {
-        fmt.Printf("Some error %v", err)
-        return
-    }
-    fmt.Fprintf(conn, "Hi UDP Server, How are you doing?")
-    _, err = bufio.NewReader(conn).Read(p)
-    if err == nil {
-        fmt.Printf("%s\n", p)
-    } else {
-        fmt.Printf("Some error %v\n", err)
-    }
-    conn.Close()
+	init_client_server()
+	fmt.Printf(" UDP Client TESTING ! \n")
+
 }
